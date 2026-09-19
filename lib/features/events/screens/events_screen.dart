29b5b871be90a79aa88c18448e17d07b5acbb7c7
Batın Dağlı <batindagli1106@gmail.com>
2026-09-19 16:00:07 +0300

    Fix CI test failures: add missing EventStatus import in EventsScreen

--- lib/features/events/screens/events_screen.dart
+++ lib/features/events/screens/events_screen.dart
@@ -4,6 +4,7 @@ import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_spacing.dart';
 import '../../../shared/widgets/states/states.dart';
 import '../models/event_model.dart';
+import '../models/event_status.dart';
 import '../services/event_service.dart';
 import '../widgets/event_card.dart';
 import 'create_event_screen.dart';
@@ -140,6 +141,7 @@ class _EventsScreenState extends State<EventsScreen> {
   }
 
   /// UI testi ve inceleme için örnek etkinlik listesi.
+  // ignore: unused_element
   static List<EventModel> _getSampleEvents() {
     return [
       EventModel(
